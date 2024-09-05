5f4208304a67fcc5e4d027f0f6882c33352a4665
huanghch <hc870960308@gmail.com>
2024-09-05 22:53:44 +0800

    Player, PlayerData, PlayerManager

---
+++ Server/scripts/logic/PlayerData.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server.logic
+{
+    public class PlayerData
+    {
+        // 金币
+        public int coin = 0;
+        // 记事本
+        public string text = "new text";
+    }
+}
