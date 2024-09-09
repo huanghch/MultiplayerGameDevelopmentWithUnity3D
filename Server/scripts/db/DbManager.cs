06eeb21531eacd9edb34628e7ffa48c3662de40c
huanghch <hc870960308@gmail.com>
2024-09-09 21:43:31 +0800

    服务器注册

--- Server/scripts/db/DbManager.cs
+++ Server/scripts/db/DbManager.cs
@@ -2,6 +2,7 @@
 using System.Data;
 using System.Security.Permissions;
 using MySql.Data.MySqlClient;
+using System.Text.RegularExpressions;
 
 namespace Server.db
 {
@@ -27,6 +28,81 @@ namespace Server.db
             catch (Exception e)
             {
                 Console.WriteLine("[数据库]connect fail, " + e.Message);
+                return false;
+            }
+        }
+
+        //判定安全字符串
+        private static bool IsSafeString(string str)
+        {
+            return !Regex.IsMatch(str, @"[-|;|,|\/|\(|\)|\[|\]|\}|\{|%|@|\*|!|\']");
+        }
+
+        // 是否存在该用户
+        public static bool IsAccountExist(string id)
+        {
+            // 防止SQL注入
+            if(!DbManager.IsSafeString(id))
+            {
+                return false;
+            }
+
+            // SQL语句
+            string s = string.Format("select * from account where id = '{0}';", id);
+            // 查询
+            try
+            {
+                MySqlCommand cmd = new MySqlCommand(s, mysql);
+                MySqlDataReader dataReader = cmd.ExecuteReader();
+                bool hasRows = dataReader.HasRows;
+                dataReader.Close();
+                return !hasRows;
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine("[数据库] IsSafeString err, " + e.Message);
+                return false;
+            }
+
+            return false;
+        }
+
+        // 注册
+        public static bool Register(string id, string pw)
+        {
+            if (!DbManager.IsSafeString(id))   
+            {
+                Console.WriteLine("[数据库] Register fail, id is not safe");
+                return false;
+            }
+
+            if(!DbManager.IsSafeString(pw))
+            {
+                Console.WriteLine("[数据库] Register fail, password is not safe");
+                return false;
+            }
+
+
+            // 能否注册
+            if(!IsAccountExist(id))
+            {
+                Console.WriteLine("[数据库] Register fail, id exist");
+                return false;
+            }
+            
+
+            // 写入数据库User表
+            string s = string.Format("insert into account set id = '{0}', pw = '{1}';", id, pw);
+
+            try
+            {
+                MySqlCommand cmd = new MySqlCommand(s, mysql);
+                cmd.ExecuteNonQuery();
+                return true;
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine("[数据库] Register fail " + e.Message);
                 return false;
             }
         }
